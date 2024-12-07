267fcb811d3cbb5c14e7668ae731bfa3ee842d4e
samsonmking <samson@samsonmking.com>
2024-12-07 17:33:26 -0500

    fix: Compatability with Fedora

--- install_jetbrains_mono_nerdfont.sh
+++ install_jetbrains_mono_nerdfont.sh
@@ -1,7 +1,12 @@
 # /bin/bash
 set -e
 
-FONT_PATH=~/.local/share/fonts
+# https://docs.fedoraproject.org/en-US/quick-docs/fonts/
+FONT_PATH=~/.local/share/fonts/jetbrains-mono-nerdfont
 
 mkdir -p $FONT_PATH
-cd $FONT_PATH && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/patched-fonts/JetBrainsMono/Ligatures/Regular/JetBrainsMonoNerdFontMono-Regular.ttf
+cd $FONT_PATH && \
+    curl -fLO \
+    https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/patched-fonts/JetBrainsMono/Ligatures/Regular/JetBrainsMonoNerdFontMono-Regular.ttf
+
+fc-cache -v
