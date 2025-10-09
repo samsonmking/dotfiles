22d0eea4e6d3acb7694c6cfab352a97a5628c9e5
Samson King <samson.king@benchling.com>
2025-10-09 15:10:23 -0400

    feat: Bind nvim fzf_lua complete_file to <C-x><C-f>

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -129,6 +129,12 @@ vim.cmd.colorscheme "vscode"
 vim.api.nvim_set_keymap("n", "<C-\\>", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {})
 vim.api.nvim_set_keymap("n", "<C-p>", [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
 vim.api.nvim_set_keymap("n", "<C-g>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {})
+vim.keymap.set({ "i" }, "<C-x><C-f>",
+  function()
+    FzfLua.complete_file({
+      winopts = { preview = { hidden = true } }
+    })
+  end, { silent = true, desc = "Fuzzy complete file" })
 
 -- nvim-tree configuration
 require("nvim-tree").setup({
