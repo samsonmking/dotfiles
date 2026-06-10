a102c87f87132553233c4ab3dfadf39ce14674c4
Samson King <samson.king@benchling.com>
2026-06-10 10:35:12 -0400

    fix: [nvim] use live_grep_native for faster grep in large repos

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -150,7 +150,7 @@ vim.cmd("colorscheme onedark_dark")
 -- fzf-lua configuration
 vim.api.nvim_set_keymap("n", "<C-\\>", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {})
 vim.api.nvim_set_keymap("n", "<C-p>", [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
-vim.api.nvim_set_keymap("n", "<C-g>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-g>", [[<Cmd>lua require"fzf-lua".live_grep_native()<CR>]], {})
 vim.keymap.set({ "i" }, "<C-x><C-f>",
 function()
   FzfLua.complete_file({
