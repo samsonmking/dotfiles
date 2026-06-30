226ed53c9776ceb58ef37871ec06f298f7fecd89
Samson King <samson.king@benchling.com>
2026-06-30 11:10:47 -0400

    fix: [nvim] prevent C-\ buffer and tmux-navigator clash

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -1,5 +1,17 @@
 return {
   { "preservim/nerdcommenter" },
   { "tpope/vim-sleuth" },
-  { "christoomey/vim-tmux-navigator" },
+  {
+    "christoomey/vim-tmux-navigator",
+    init = function()
+      -- Disable default mappings so <C-\> stays free for fzf-lua buffers.
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+    config = function()
+      vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { silent = true })
+      vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { silent = true })
+      vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { silent = true })
+      vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { silent = true })
+    end,
+  },
 }
