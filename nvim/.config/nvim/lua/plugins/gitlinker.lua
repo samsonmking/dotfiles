bad8d6150814d20f6550b4760f19f0bc025e7f34
Samson King <samson.king@benchling.com>
2026-06-30 11:40:16 -0400

    feat: [nvim] configure gitlinker

---
+++ nvim/.config/nvim/lua/plugins/gitlinker.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>gl", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git permlink to clipboard" },
+    },
+  },
+}
