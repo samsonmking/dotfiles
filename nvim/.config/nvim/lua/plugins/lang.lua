6b046d9d37515c9e4bc59a9ed40d58713f720471
Samson King <samson.king@benchling.com>
2026-08-21 15:07:05 -0400

    feat: [nvim] setup diagnostics bindings + float

--- nvim/.config/nvim/lua/plugins/lang.lua
+++ nvim/.config/nvim/lua/plugins/lang.lua
@@ -44,7 +44,29 @@ return {
           end
           map("gd", vim.lsp.buf.definition, "Go to definition")
           map("gD", vim.lsp.buf.declaration, "Go to declaration")
-          map("<leader>e", vim.diagnostic.open_float, "Show diagnostics")
+          vim.diagnostic.config({
+            update_in_insert = false,
+            severity_sort = true,
+            float = { border = "rounded", source = "if_many" },
+            underline = { severity = { min = vim.diagnostic.severity.WARN } },
+
+            -- Can switch between these as you prefer
+            virtual_text = true, -- Text shows up at the end of the line
+            virtual_lines = false, -- Text shows up underneath the line, with virtual lines
+
+            -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
+            jump = {
+              on_jump = function(_, bufnr)
+                vim.diagnostic.open_float({
+                  bufnr = bufnr,
+                  scope = "cursor",
+                  focus = false,
+                })
+              end,
+            },
+          })
+
+          vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
         end,
       })
     end,
